e7034f9ecbbbfa00cd87b14b0cbad2e7d48f1df0
helix123 <kevintappe@gmx.de>
2015-08-11 19:56:13 +0200

    Update plm_fixes_v1.4-0_kt.R

--- plm_fixes_v1.4-0_kt.R
+++ plm_fixes_v1.4-0_kt.R
@@ -5,7 +5,7 @@
 ## F statistic when a user specified variance-covariance matrix is supplied (for robust inference)
 #  [see http://stackoverflow.com/questions/31163353/computing-f-statistic-with-user-supplied-covariance-matrix]
 #
-#  Note: Forumula interface not fixed
+#  Note: Formula interface not fixed
 
 ## Durbin-Watson test respecting panel structure
 #  [see http://stackoverflow.com/questions/31894055/pdwtest-from-plm-with-wrong-p-value-for-pooled-ols-durbin-watson-test-for-autoc]
