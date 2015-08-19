a3fa3c2b4eb0ecb76cc8f7f119118a8e57646add
helix123 <kev_t@gmx.de>
2015-08-19 16:12:54 +0200

    pwfdtest: added informative message if wrong model is passed
    fd model needs to be passed

--- plm_fixes_v1.4-0_kt.R
+++ plm_fixes_v1.4-0_kt.R
@@ -2,7 +2,7 @@
 # no warranty
 # License: GPL
 #
-# Version of this file 0.4-1
+# Version of this file 0.4-2
 #
 #
 # Please find this file also at https://github.com/helix123/plm_fixes
@@ -742,5 +742,102 @@ lead.pseries <- function(x, k = 1, ...) {
   if (length(k) > 1) colnames(ret) <- k
   return(ret)
 }
+
+#### pwfdtest.panelmodel
+##
+## added: informative informative message if wrong model is passed
+
+pwfdtest.panelmodel <- function(x, ..., h0=c("fd","fe")) {
+  ## first-difference-based serial correlation test for panel models
+  ## ref.: Wooldridge (2003), par. 10.6 
+  if (plm:::describe(x, "model") != "fd") stop("pwfdtest only relevant for first-differenced models") ## added this line
+  
+  if(!require(car)) stop("Library 'car' is needed")
+  
+  ## fetch fd residuals
+  FDres <- resid(x)
+  ## indices (full length! must reduce by 1st time period)
+  ## this is an ad-hoc solution for the fact that the 'fd' model
+  ## carries on the full indices while losing the first time period
+  index <- attr(model.frame(x), "index")
+  time <- as.numeric(index[[2]])
+  id <- as.numeric(index[[1]])
+  
+  ## fetch dimensions and adapt to those of indices
+  pdim <- pdim(x)
+  n <- pdim$nT$n
+  
+  
+  ## (re)create groupwise-separated index from 1 to nT 
+  ## - dropping first time period
+  ## - correcting Ti=Ti+1
+  Ti <- pdim$Tint$Ti-1
+  
+  redind <- vector("list",n)
+  tfirst <- 0
+  for(i in 1:n) {
+    redind[[i]] <- (tfirst+2):(tfirst+Ti[i]+1)
+    tfirst <- max(redind[[i]])
+  }
+  ## reduce indices by 1st time period
+  redind <- unlist(redind)
+  time <- time[redind]
+  id <- id[redind]
+  
+  N <- length(FDres)
+  
+  FDres.1 <- c(NA,FDres[1:(N-1)])
+  
+  lagid  <-  id-c(NA,id[1:(N-1)])
+  
+  FDres.1[lagid!=0] <- NA
+  
+  
+  ## make (panel) dataframe for auxiliary regression
+  auxdata <- as.data.frame(cbind(id,time))
+  auxdata$FDres <- FDres
+  auxdata$FDres.1 <- FDres.1
+  ## pooling model FDres vs. lag(FDres),
+  ## with intercept (might as well do it w.o.)
+  auxmod <- plm(FDres ~ FDres.1, na.omit(auxdata), model = "pooling")
+  
+  switch(match.arg(h0), 
+         fd = {h0des<-"differenced"
+         ## theoretical rho under H0: no serial 
+         ## corr. in differenced errors is 0
+         rho.H0 <- 0},
+         fe = {h0des<-"original"
+         ## theoretical rho under H0: no serial 
+         ## corr. in original errors is -0.5
+         rho.H0 <- -0.5})
+  
+  ## test H0: rho=rho.H0 with HAC t-test (HC0-3 parm may be passed)
+  myvcov <- function(x) vcovHC(x, method="arellano", ...)
+  
+  myH0 <- paste("FDres.1 = ", as.character(rho.H0), sep="")
+  lhtest <- linearHypothesis(model=auxmod, myH0, vcov.=myvcov, ...)
+  
+  ##(insert usual htest features)  
+  FDARstat <- lhtest[2,3]
+  names(FDARstat) <- dimnames(lhtest)[[2]][3] 
+  if (names(FDARstat)=="Chisq") names(FDARstat) <- "chisq"
+  ## this is either 'F' or 'Chisq' and is the name of 3rd
+  ## column because we are supplying a vcov matrix
+  pFDAR<-lhtest[2,4]
+  
+  dname <- paste(deparse(substitute(x)))
+  RVAL <- list(statistic = FDARstat, parameter = NULL,
+               method = "Wooldridge's first-difference test for serial correlation in panels",
+               alternative = paste("serial correlation in", h0des, "errors"),
+               p.value = pFDAR,
+               data.name =   dname)
+  class(RVAL) <- "htest"
+  return(RVAL)
+  
+}
+
+
+
+
 
 
